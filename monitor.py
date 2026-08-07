b7f109e48bf82aba14d1a61e6d1bf9f6a5f6a1c6
csafatg001hkb88-design <csafatg001.hkb88@gmail.com>
2026-08-08 06:00:17 +0700

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import os
 
 BOT_TOKEN = "8831943364:AAHKaZEYWo0RKi3YJtNW_rfW9uo0vC1Em8E"
 CHAT_ID = "7844730036"
@@ -14,6 +16,16 @@ WEBSITES = [
     }
 ]
 
+STATUS_FILE = "status.json"
+
+if os.path.exists(STATUS_FILE):
+    with open(STATUS_FILE, "r") as f:
+        status_lama = json.load(f)
+else:
+    status_lama = {}
+
+status_baru = {}
+
 for site in WEBSITES:
     try:
         response = requests.get(
@@ -25,15 +37,40 @@ for site in WEBSITES:
         )
 
         if response.status_code == 200:
+
             print(f'{site["nama"]} ONLINE')
+            status_baru[site["nama"]] = "ONLINE"
+
+            if status_lama.get(site["nama"]) == "DOWN":
+
+                pesan = f"""🟢 DOMAIN ONLINE KEMBALI
+
+Website : {site["nama"]}
+Domain  : {site["url"]}
+
+✅ Website sudah dapat diakses kembali.
+"""
+
+                r = requests.get(
+                    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+                    params={
+                        "chat_id": CHAT_ID,
+                        "text": pesan
+                    }
+                )
+
+                print(r.text)
 
         else:
+
             print(f'{site["nama"]} DOWN')
+            status_baru[site["nama"]] = "DOWN"
 
             pesan = f"""🔴 DOMAIN DOWN
 
 Website : {site["nama"]}
 Domain  : {site["url"]}
+
 Status  : {response.status_code}
 """
 
@@ -47,24 +84,29 @@ Status  : {response.status_code}
 
             print(r.text)
 
-    else:
+    except Exception as e:
 
-    print(f"{nama} DOWN")
-    status_baru[nama] = "DOWN"
+        print(e)
+        status_baru[site["nama"]] = "DOWN"
 
-    pesan = (
-        "🔴 DOMAIN DOWN\n\n"
-        f"🌐 Website : {nama}\n"
-        f"🔗 Domain : {url}\n\n"
-        f"❌ HTTP Status : {response.status_code}"
-    )
+        pesan = f"""🔴 DOMAIN DOWN
 
-    requests.get(
-        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-        params={
-            "chat_id": CHAT_ID,
-            "text": pesan
-        }
-    )
+Website : {site["nama"]}
+Domain  : {site["url"]}
+
+Error :
+{e}
+"""
+
+        r = requests.get(
+            f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+            params={
+                "chat_id": CHAT_ID,
+                "text": pesan
+            }
+        )
 
         print(r.text)
+
+with open(STATUS_FILE, "w") as f:
+    json.dump(status_baru, f)
