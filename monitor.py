ab5c54562c32c982f23fe78afeceba306126208c
csafatg001hkb88-design <csafatg001.hkb88@gmail.com>
2026-08-08 05:16:49 +0700

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,9 +1,9 @@
 import requests
 
-BOT_TOKEN = "8831943364:AAHKaZEYWo0RKi3YJtNW_rfW9uo0vC1Em8E";
-CHAT_ID = "7844730036";
+BOT_TOKEN = "8831943364:AAHKaZEYWo0RKi3YJtNW_rfW9uo0vC1Em8E"
+CHAT_ID = "7844730036"
 
-websites = [
+WEBSITES = [
     {
         "nama": "afatogel",
         "url": "https://afatogel.com"
@@ -14,22 +14,30 @@ websites = [
     }
 ]
 
-for site in websites:
+for site in WEBSITES:
     try:
-        response = requests.get(site["url"], timeout=10)
+        response = requests.get(
+            site["url"],
+            timeout=10,
+            headers={
+                "User-Agent": "Mozilla/5.0"
+            }
+        )
 
         if response.status_code == 200:
             print(f'{site["nama"]} ONLINE')
+
         else:
             print(f'{site["nama"]} DOWN')
 
-            pesan = (
-                f"🔴 Domain DOWN\n\n"
-                f"Website: {site['nama']}\n"
-                f"Domain: {site['url']}"
-            )
+            pesan = f"""🔴 DOMAIN DOWN
 
-            requests.get(
+Website : {site["nama"]}
+Domain  : {site["url"]}
+Status  : {response.status_code}
+"""
+
+            r = requests.get(
                 f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
                 params={
                     "chat_id": CHAT_ID,
@@ -37,19 +45,27 @@ for site in websites:
                 }
             )
 
-    except Exception:
-        print(f'{site["nama"]} DOWN')
+            print(r.text)
 
-        pesan = (
-            f"🔴 Domain DOWN\n\n"
-            f"Website: {site['nama']}\n"
-            f"Domain: {site['url']}"
-        )
+    except Exception as e:
 
-        requests.get(
+        print(e)
+
+        pesan = f"""🔴 DOMAIN DOWN
+
+Website : {site["nama"]}
+Domain  : {site["url"]}
+
+Error:
+{e}
+"""
+
+        r = requests.get(
             f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
             params={
                 "chat_id": CHAT_ID,
                 "text": pesan
             }
         )
+
+        print(r.text)
